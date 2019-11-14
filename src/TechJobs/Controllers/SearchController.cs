7e92db0612c1c40a43cb29ba1720cc20a5bdef72
Elizabeth Reiher <tonomoshia@gmail.com>
2019-11-13 20:56:18 -0500

    add pseudcode to each TODO laying out needed steps

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,21 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm = "")
+        {
+            // Create a list to store queried results to be returned
+
+            if (searchType.Equals("all"))
+            {
+                //use FindByValue passing in the searchTerm and store the values in the list
+            }
+            else
+            {
+                //Use the searchTerm and the searchType and use the FindByColumnAndValue method and save that to the list
+            }
+            //set the value of ViewBag.columns property to the column Choices of the ListController class
+            //then set the ViewBag.jobs property to the list
+            //then return the index view
+        }
     }
 }
