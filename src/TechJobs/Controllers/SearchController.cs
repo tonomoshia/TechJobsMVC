17bcb96f7ed49b00d92c99c4397a66a1d6d57cc1
Elizabeth Reiher <tonomoshia@gmail.com>
2019-11-20 20:34:13 -0500

    implement todo #1: create a Results action method to process

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -19,18 +19,27 @@ namespace TechJobs.Controllers
         public IActionResult Results(string searchType, string searchTerm = "")
         {
             // Create a list to store queried results to be returned
-
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            //search all
             if (searchType.Equals("all"))
             {
-                //use FindByValue passing in the searchTerm and store the values in the list
+                // use FindByValue passing in the searchTerm and store the values in the list
+                ViewBag.Jobs = JobData.FindByValue(searchTerm);
             }
+            // search by category
             else
             {
                 //Use the searchTerm and the searchType and use the FindByColumnAndValue method and save that to the list
+                ViewBag.Jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
             }
-            //set the value of ViewBag.columns property to the column Choices of the ListController class
-            //then set the ViewBag.jobs property to the list
+            // set the value of ViewBag.columns property to the column Choices of the ListController class
+            // ViewBag.columns = ListController.columnChoices;
+            // then set the ViewBag.jobs property to the list
+
             //then return the index view
+            //return View();
+            return View("~/Views/Search/Index.cshtml");
         }
     }
 }
